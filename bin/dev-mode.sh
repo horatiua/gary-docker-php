b40b7460e3c154eea70065657f1a29359296837b
Horatiu Andrei <horatiu.andrei@gmail.com>
2025-04-08 13:26:50 +0530

    Symfony 6.4.*

--- bin/dev-mode.sh
+++ bin/dev-mode.sh
@@ -4,4 +4,4 @@ XDEBUG_MODE=${XDEBUG_MODE:-off} \
   docker compose \
   -f docker-compose.yaml \
   -f docker-compose.dev.yaml \
-  --env-file .env.local up "$@"
+  --env-file ./app/.env.local up "$@"
